3aca15fac3f9865d877045f95b97a3eb9864ff90
goretaivor <ivor.goreta@primus.com.al>
2024-03-19 15:12:55 +0100

    Initial commit

---
+++ src/test/java/com/ivoronline/springboot_demo2/SpringbootDemo2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ivoronline.springboot_demo2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootDemo2ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}

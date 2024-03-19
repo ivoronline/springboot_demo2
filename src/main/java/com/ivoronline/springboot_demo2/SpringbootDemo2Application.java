201d9f303018f163c8809b53820b9939011495f9
goretaivor <ivor.goreta@primus.com.al>
2024-03-19 15:23:43 +0100

    ss

--- src/main/java/com/ivoronline/springboot_demo2/SpringbootDemo2Application.java
+++ src/main/java/com/ivoronline/springboot_demo2/SpringbootDemo2Application.java
@@ -7,7 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class SpringbootDemo2Application {
 
     public static void main(String[] args) {
-        SpringApplication.run(SpringbootDemo2Application.class, args);
+        SpringApplication.run(SpringbootDemo2Application.class, args); //ytt
     }
 
 }
